ac81fb41f85d5340f12ec1a8a30d4b032289705d
adityachvhan <adityachavhan11@gmail.com>
2026-05-14 15:12:43 +0530

    Initial project setup

---
+++ src/main/java/com/gitdemo/git_upload_demo/GitUploadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gitdemo.git_upload_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUploadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUploadDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitdemo/git_upload_demo/GitUploadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitdemo.git_upload_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitUploadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
